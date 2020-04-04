07b600b94e6476d31fcdec5f2a7ea3054e76f227
Allan Farca <allan.farca@gmail.com>
2020-04-04 01:25:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime? (number)
-  
+    if (number < 0)
+      return false  
   half_number = number / 2
   i = 1
   while (i < half_number)
-    if (number < 0)
-      return false
+
     elsif (number % i == 0)
       return false
     else
